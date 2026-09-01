ddfb73f5e4fc042aeb85b70035631bb07adc68b9
satoshi620519 <trinity.japan870@gmail.com>
2026-09-01 09:27:52 +0900

    test(db): extend committed migration sequence through administration roles

--- packages/database/src/migrations.test.ts
+++ packages/database/src/migrations.test.ts
@@ -28,6 +28,7 @@ describe('repository migration sequence', () => {
         '0003_create_verification.sql',
         '0005_create_email_outbox_messages.sql',
         '0006_add_email_outbox_terminal_failure.sql',
+        '0007_create_administration_roles.sql',
       ]),
     ).toEqual([
       '0001_create_accounts.sql',
@@ -36,6 +37,7 @@ describe('repository migration sequence', () => {
       '0004_create_safety_enforcements.sql',
       '0005_create_email_outbox_messages.sql',
       '0006_add_email_outbox_terminal_failure.sql',
+        '0007_create_administration_roles.sql',
     ]);
   });
 });
