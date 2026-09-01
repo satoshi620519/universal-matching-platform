33eb0df0fc888a39dc6e8fb1aa7001461e2a0a01
satoshi620519 <trinity.japan870@gmail.com>
2026-09-01 13:02:30 +0900

    fix(database): order email outbox migrations after table creation

--- packages/database/migrations/0006_add_email_outbox_terminal_failure.sql
+++ packages/database/migrations/0006_add_email_outbox_terminal_failure.sql
@@ -1,5 +1,2 @@
 ALTER TABLE email_outbox_messages
   ADD COLUMN failed_at TIMESTAMPTZ NULL;
-
-CREATE INDEX email_outbox_messages_status_failed_at_idx
-  ON email_outbox_messages (status, failed_at);
