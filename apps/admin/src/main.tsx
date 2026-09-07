631759c30523491987b540fd358176ace65993c6
satoshi620519 <trinity.japan870@gmail.com>
2026-09-07 16:49:17 +0900

    feat(admin): mount failed email outbox workspace

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,5 +1,6 @@
 import './moderation-console';
 import './administrative-workspace-navigation-ui';
+import './failed-email-outbox-console';
 import React, { useMemo, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createQuickLaunchWorkflow, quickLaunchSteps, type QuickLaunchDraftInput } from './quick-launch-workflow';
